6e2111cba0f59502738a223fa7c54e5e8bfd51dc
Alma <alma.morante.morales@gmail.com>
2022-07-25 12:54:31 +0200

    update

--- jsControl.js
+++ jsControl.js
@@ -1,35 +1,45 @@
+
 let readDoc = false;
 let correctMail = false;
 
 
 function checkMail(){
     let mailBar = String(document.getElementById("userMail").value);
-    let checkBox = document.getElementById("policies");
     console.log(mailBar);
     if(mailBar.includes('@idp.es')){
-        checkBox.removeAttribute('disabled');
         correctMail = true;
     }else{
-        checkBox.setAttribute('disabled','');
-        checkBox.checked = false;
         correctMail = false;
+        
     }
 }
 
 
 function checkDoc(){
     readDoc = true;
-    
 }
-
 
 
  function checkBox(){
     let check = document.getElementById("policies");
-    console.log(check.attributes);
-    if(check.getAttribute('disabled')!=null){
+    if(!correctMail){
         alert("Por favor, rellene el campo 'Email' con un correo válido (ejemplo@idp.es)");
-    }else{
+        check.disabled = true;
         check.checked = false;
+    }else if(!readDoc){
+        alert("Por favor, pulse el botón rojo para acceder y leer las políticas");
+    }else{
+        check.removeAttribute('disabled');
+        check.checked = false;
+        document.getElementById('submit').disabled = false;
     }
+ }
+ 
+ function submit(){
+    let userInput = document.getElementById('userMail').value;
+    let checkBox = document.getElementById('policies').value;
+    let fileinfo = String(userInput+";"+checkBox);
+    let content = "hello world";
+    let blob = new Blob([content], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, "usersText.txt");
  }
