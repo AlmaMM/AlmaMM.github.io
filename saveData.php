ca22885af86a5a84422ac952ed9cbd9deed6e343
Alma <alma.morante.morales@gmail.com>
2022-07-25 13:34:50 +0200

    updated

--- saveData.php
+++ saveData.php
@@ -1,7 +1,7 @@
 <?php
-$myFile = "data.json";
+$myFile = "data.txt";
 $fh = fopen($myFile, 'a') or die("can't open file");
 $stringData = $_POST["data"];
-fwrite($fh, $stringData);
+fwrite($fh, $stringData.PHP_EOL);
 fclose($fh)
 ?>
